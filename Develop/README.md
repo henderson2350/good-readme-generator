f85741c63d81a62eccb31bb4f7bca707d1c8f352
Clare Henderson <henderson2350@gmail.com>
2021-04-08 16:37:45 -0400

    markdown issues

--- Develop/README.md
+++ Develop/README.md
@@ -1,31 +1,35 @@
-# asdf
-undefined
+# asdf hello
 
-## Description
-asdf
-
-## Table of Contents
-1. [Installation](#installation)
-2. [Usage](#usage)
-3. [License](#license)
-4. [Contributing](#contributing)
-4. [Tests](#tests)
-5. [Questions](#questions)
-
-## Installation
-
-## Usage
-asdf
-
-## License
-License type: MIT
-
-## Contributing
-asdf
-
-## Tests
-
-## Questions
-# For more information contact me:
-- GitHub: https://github.com/clare  
-- My email: asdf
+        # hello
+        
+    
+        ## Description
+        asdf
+    
+        ## Table of Contents
+        1. [Installation](#installation)
+        2. [Usage](#usage)
+        3. [License](#license)
+        4. [Contributing](#contributing)
+        4. [Tests](#tests)
+        5. [Questions](#questions)
+    
+        ## Installation
+        
+    
+        ## Usage
+        
+    
+        ## License
+        License type: MIT
+    
+        ## Contributing
+        
+    
+        ## Tests
+    
+        ## Questions
+        For more information contact me:
+        - GitHub: https://github.com/asdf  
+        - My email: asdf
+        ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)(https://opensource.org/licenses/MIT)
--- Develop/index.js
+++ Develop/index.js
@@ -2,6 +2,8 @@
 const inquirer = require("inquirer")
 const fs = require("fs")
 
+var licenseLink = "test"
+
 inquirer
     .prompt(
         [{
@@ -21,7 +23,7 @@ inquirer
         },
         {
             type: "input",
-            message: "Please write a short description of your project",
+            message: "Please write a short description of your project:",
             name: "description"
         },
         {
@@ -32,60 +34,70 @@ inquirer
         },
         {
             type: "input",
-            message: "Give brief instructions to the user for using your project.",
+            message: "Give brief instructions to the user for using your project:",
             name: "usage"
         },
         {
             type: "input",
-            message: "Please list any collaborators and their github usernames.",
+            message: "Please list any collaborators and their github usernames:",
             name: "contributors"
+        },
+        {
+            type: "input",
+            message: "Please write instructions for the user to install your project:",
+            name: "installation"
         }
 
     
     ]
     ) .then((response) => {
-        console.log(response);
-        const readMePageContent = pageContent(response)
-        licenseType = response.license
-        if (licenseType === "MIT") {
-            licenseLink = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
-            console.log(licenseLink)
-        }
+            const readMePageContent = pageContent(response, licenseLink)
 
-        fs.writeFile("README.md", readMePageContent, err =>
-        err ? console.log(err) : console.log("Succesfully created README.md"))
-    })
+            fs.writeFile("README.md", readMePageContent, err =>
+            err ? console.log(err) : console.log("Succesfully created README.md"))
+        
+    }
+)
+
+const pageContent = (response) => {
+    if (response.license === "MIT") {
+        var licenseLink = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+        
+        return `# ${response.project} hello
+
+        # hello
+        
+        ## Description
+        ${response.description}
+    
+        ## Table of Contents
+        1. [Installation](#installation)
+        2. [Usage](#usage)
+        3. [License](#license)
+        4. [Contributing](#contributing)
+        4. [Tests](#tests)
+        5. [Questions](#questions)
+    
+        ## Installation
+        ${response.installation}
+    
+        ## Usage
+        ${response.usage}
+    
+        ## License
+        License type: ${response.license}
+    
+        ## Contributing
+        ${response.contributors}
+    
+        ## Tests
+    
+        ## Questions
+        For more information contact me:
+        - GitHub: https://github.com/${response.username}  
+        - My email: ${response.email}
+        ${licenseLink}`
+    }
+    }
 
 
-const pageContent = (response, licenseLink) =>
-`# ${response.project}
-${licenseLink}
-
-## Description
-${response.description}
-
-## Table of Contents
-1. [Installation](#installation)
-2. [Usage](#usage)
-3. [License](#license)
-4. [Contributing](#contributing)
-4. [Tests](#tests)
-5. [Questions](#questions)
-
-## Installation
-
-## Usage
-${response.usage}
-
-## License
-License type: ${response.license}
-
-## Contributing
-${response.contributors}
-
-## Tests
-
-## Questions
-# For more information contact me:
-- GitHub: https://github.com/${response.username}  
-- My email: ${response.email}`
